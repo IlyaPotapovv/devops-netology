7566c9de67d0c424e0d185ae51d0d5beeb3165eb
ilya.potapov <ilyapotapovv1997@yandex.ru>
2021-11-05 13:44:11 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,13 +1,31 @@
+cat rebase.sh
 #!/bin/bash
 # display command line options
-
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> dc4688f... git 2.3 rebase @ instead *
     count=$(( $count + 1 ))
 done
 
-echo "====="
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
 
 
 
